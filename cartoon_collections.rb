6341dceb122d68f1f7cc07e42ca937f0ac46a049
Jantastication O <georginaoregx@gmail.com>
2018-10-03 22:16:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,12 +5,18 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |character|
+  character.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
